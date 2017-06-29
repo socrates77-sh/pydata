9020f7c65de1b0f87e287709af8e807ab53b5f35
socrates77 <zhwenrong@sina.com>
2017-06-29 08:58:59 +0800

    learn ch02 @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

---
+++ ch02/bit_ly.py
@@ -0,0 +1,48 @@
+
+import json
+from collections import defaultdict
+from collections import Counter
+
+
+def top_counts(count_dict, n=10):
+    value_key_pairs = [(count, tz) for tz, count in count_dict.items()]
+    value_key_pairs.sort()
+    return value_key_pairs[-n:]
+
+
+def get_counts2(sequence):
+    counts = defaultdict(int)
+    for x in sequence:
+        counts[x] += 1
+    return counts
+
+
+def get_counts(sequence):
+    counts = {}
+    for x in sequence:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
+path = 'ch02/usagov_bitly_data2012-03-16-1331923249.txt'
+# a = open(path).readline()
+# print(a)
+
+records = [json.loads(line) for line in open(path)]
+# print(open(path))
+print(records[0])
+print(records[0]['tz'])
+
+time_zones = [rec['tz'] for rec in records if 'tz' in rec]
+# print(time_zones[:10])
+
+# counts = get_counts2(time_zones)
+# # print(counts)
+
+# print(top_counts(counts))
+
+counts = Counter(time_zones)
+# print(counts)
+print(counts.most_common(10))
