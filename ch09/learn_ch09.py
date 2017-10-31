61c23a26e913ab0d246563dc5818b0a6601f2bf2
socrates77 <zhwenrong@sina.com>
2017-10-31 08:41:57 +0800

    learn ch09 @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- ch09/learn_ch09.py
+++ ch09/learn_ch09.py
@@ -45,4 +45,25 @@ people.groupby(map_series, axis=1).count()
 
 people.groupby(len).sum()
 
-#271
+grouped = df.groupby('key1')
+grouped['data1'].quantile(0.9)
+
+
+def peak_to_peak(arr):
+    return arr.max() - arr.min()
+
+grouped.agg(peak_to_peak)
+grouped.describe()
+
+tips = pd.read_csv('tips.csv')
+tips['tip_pct'] = tips['tip'] / tips['total_bill']
+grouped = tips.groupby(['sex', 'smoker'])
+grouped_pct = grouped['tip_pct']
+grouped_pct.agg('mean')
+grouped_pct.agg(['mean', 'std', peak_to_peak])
+grouped_pct.agg([('foo', 'mean'), ('bar', np.std)])
+functions = ['count', 'mean', 'max']
+result = grouped['tip_pct', 'total_bill'].agg(functions)
+grouped.agg({'tip': np.max, 'size': 'sum'})
+grouped.agg({'tip_pct': ['min', 'max', 'mean', 'std'], 'size': 'sum'})
+#287
