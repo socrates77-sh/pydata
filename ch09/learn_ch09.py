39db787beb5bddb5674afb3744136c4c28dc5c84
socrates77 <zhwenrong@sina.com>
2017-11-03 19:46:07 +0800

    learn ch09 @office
    Signed-off-by: socrates77 <zhwenrong@sina.com>

--- ch09/learn_ch09.py
+++ ch09/learn_ch09.py
@@ -66,4 +66,87 @@ functions = ['count', 'mean', 'max']
 result = grouped['tip_pct', 'total_bill'].agg(functions)
 grouped.agg({'tip': np.max, 'size': 'sum'})
 grouped.agg({'tip_pct': ['min', 'max', 'mean', 'std'], 'size': 'sum'})
-#287
+
+k1_means = df.groupby('key1').mean().add_prefix('mean_')
+pd.merge(df, k1_means, left_on='key1', right_index=True)
+
+key = ['one', 'two', 'one', 'two', 'one']
+people.groupby(key).mean()
+people.groupby(key).transform(np.mean)
+
+
+def demean(arr):
+    return arr - arr.mean()
+
+demeaned = people.groupby(key).transform(demean)
+demeaned.groupby(key).mean()
+
+
+def top(df, n=5, column='tip_pct'):
+    return df.sort_values(by=column)[-n:]
+
+top(tips, n=6)
+
+tips.groupby('smoker').apply(top)
+tips.groupby(['smoker', 'day']).apply(top, n=1, column='total_bill')
+result = tips.groupby('smoker')['tip_pct'].describe()
+result.unstack('smker')
+
+
+def f = lambda x: x.describe()
+grouped.apply(f)
+
+tips.groupby('smoker', group_keys=False).apply(top)
+
+frame = DataFrame({'data1': np.random.randn(1000),
+                   'data2': np.random.randn(1000)})
+factor = pd.cut(frame.data1, 4)
+
+
+def get_stats(group):
+    return {'min': group.min(), 'max': group.max(),
+            'count': group.count(), 'mean': group.mean()}
+
+grouped = frame.data2.groupby(factor)
+grouped.apply(get_stats).unstack()
+
+grouping = pd.qcut(frame.data1, 10, labels=False)
+grouped = frame.data2.groupby(grouping)
+grouped.apply(get_stats).unstack()
+
+s = Series(np.random.randn(6))
+s[::2] = np.nan
+s.fillna(s.mean())
+
+states = ['Ohio', 'New York', 'Vermont', 'Floria',
+          'Oregon', 'Nevada', 'Clifornia', 'Idaho']
+group_key = ['East'] * 4 + ['West'] * 4
+data = Series(np.random.randn(8), index=states)
+data[['Vermont', 'Nevada', 'Idaho']] = np.nan
+data.groupby(group_key).mean()
+
+
+def fill_mean = lambda g: g.fillna(g.mean())
+data.groupby(group_key).apply(fill_mean)
+
+fill_values = {'East': 0.5, 'West': -1}
+
+
+def fill_func = lambda g: g.fillna(fill_func[g.name])
+data.groupby(group_key).apply(fill_func)
+
+suits = ['H', 'S', 'C', 'D']
+card_val = (list(range(1, 11)) + [10] * 3) * 4
+base_names = ['A'] + list(range(2, 11)) + ['J', 'Q', 'K']
+cards = []
+for suit in suits:
+    cards.extend(str(num) + suit for num in base_names)
+deck = Series(card_val, index=cards)
+
+
+def draw(deck, n=5):
+    return deck.take(np.random.permutation(len(deck))[:n])
+
+draw(deck)
+
+#296
